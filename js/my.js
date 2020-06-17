92aa74da339529d9fed4d88999c289a55f51f7c8
Ryuji-commit <56810015+Ryuji-commit@users.noreply.github.com>
2020-06-17 17:21:19 +0900

    ひと段落

--- js/my.js
+++ js/my.js
@@ -10,16 +10,25 @@ $(function(){
     };
 
     connection.onmessage = function(e) {
-        let msg = e.data;
-        $("#screen").text(msg);
+        var msg = e.data;
+
+        var top_max = $(window).height()*0.7;
+        var left_max = $(window).width()*0.9;
+        var top = Math.floor(Math.random() * top_max);
+        var left = Math.floor(Math.random() * left_max);
+
+        var insert_html = $('<div class="chat-message"><p>' + msg + '</p></div>').hide().fadeIn(500).offset({ top: top, left: left }).delay(10000).fadeOut('slow');
+        $("#screen").append(insert_html);
     };
 
     connection.onclose = function() {
         console.log("コネクションを終了しまいた。");
+        $("#screen").remove();
     };
 });
 
 $('#submit').on('click', function() {
     var msg = $("#textarea1").val();
+    $("#textarea1").val('');
     connection.send(msg);
 });
