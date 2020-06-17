cd6f8fc4a26a31d0c9d6e83e01b09157a55f15ba
Ryuji-commit <56810015+Ryuji-commit@users.noreply.github.com>
2020-06-17 11:18:01 +0900

    first commit

---
+++ my.js
@@ -0,0 +1,24 @@
+$(function(){
+    connection = new WebSocket('wss://echo.websocket.org');
+
+    connection.onopen = function(e) {
+        console.log("コネクションを開始しまいた。");
+    };
+
+    connection.onerror = function(error) {
+        console.log("エラーが発生しました。");
+    };
+
+    connection.onmessage = function(e) {
+        let msg = e.data;
+        document.getElementById("#screen").value = msg;
+    };
+
+    connection.onclose = function() {
+        console.log("コネクションを終了しまいた。");
+    };
+});
+
+$('#submit').on('click', function() {
+    connection.send(document.getElementById("#textarea1").value);
+});
